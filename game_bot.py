c2fba50d6eba51a5a88bfa4a84513dd9ba7cb2cf
Lucian-y02 <vigods125@gmail.com>
2022-05-05 23:33:41 +0300

    Load and inventory

--- game_bot.py
+++ game_bot.py
@@ -54,6 +54,7 @@ class GameBot:
         self.save = self.save_data[save_number]
         self.now_node = self.chapter_data[self.save["node_name"]]
         self.now_node_name = self.save["node_name"]
+        self.inventory = self.save["inventory"]
         self.show_node = True
         self.give_command(update, context)
 
@@ -70,6 +71,7 @@ class GameBot:
         else:
             self.function_data[self.now_node["type"]][0](update, context, update.message.text)
 
+    # Take answer ---------------------------------------------------------------------------------
     def action_take_answer(self, update, context, answer):
         if answer not in self.now_node["answers"]:
             update.message.reply_text("Неверно указан номер действия!")
@@ -77,7 +79,7 @@ class GameBot:
             key_next_node = self.now_node["answers"][answer]["next_node"]
             if key_next_node == "save":
                 with open("save_data.json", "w", encoding="utf-8") as file:
-                    while str(save_number := randrange(1, 1000)) in self.save_data:
+                    while str(save_number := randrange(1000, 10000)) in self.save_data:
                         pass
                     self.save_data[str(save_number)] = {
                         "node_name": self.now_node_name,
@@ -97,8 +99,8 @@ class GameBot:
 
     def password_take_answer(self, update, context, answer):
         if answer == "отмена":
-            self.now_node = self.chapter_data[self.now_node["prev_node"]]
             self.now_node_name = self.now_node["prev_node"]
+            self.now_node = self.chapter_data[self.now_node["prev_node"]]
             self.show_node = True
             self.give_command(update, context)
         elif answer != self.now_node["right_password"]:
@@ -106,24 +108,31 @@ class GameBot:
         else:
             key_next_node = self.now_node["next_node"]
             if key_next_node != "null":
-                self.now_node = self.chapter_data[key_next_node]
                 self.now_node_name = key_next_node
+                self.now_node = self.chapter_data[key_next_node]
                 self.show_node = True
                 self.give_command(update, context)
             else:
                 update.message.reply_text("Квест закончен!")
 
     def use_take_answer(self, update, context, answer):
-        if answer == "1":
-            pass
+        if answer == "1" and self.now_node["need_thing"] in self.inventory:
+            self.inventory.remove(self.now_node["need_thing"])
+            self.now_node_name = self.now_node["next_node"]
+            self.now_node = self.chapter_data[self.now_node["next_node"]]
+            self.show_node = True
+            self.give_command(update, context)
+        elif answer == "1":
+            update.message.reply_text("У вас нет подходящего предмета!")
         elif answer == "2":
-            self.now_node = self.chapter_data[self.now_node["prev_node"]]
             self.now_node_name = self.now_node["prev_node"]
+            self.now_node = self.chapter_data[self.now_node["prev_node"]]
             self.show_node = True
             self.give_command(update, context)
         else:
             update.message.reply_text("Неверно указан номер действия!")
 
+    # Show node -----------------------------------------------------------------------------------
     def show_action(self, update, context):
         answers = "".join(list(map(lambda x:
                                    f'{x}) {self.now_node["answers"][x]["text"]}\n',
